4268295b96f996ede85f5849dca974855315cad1
Alexis-Roman <104246596+Alexis-Roman@users.noreply.github.com>
2026-01-10 17:29:55 +0800

    added marquee bg

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import AboutMeScreen from "../Screen/AboutMeScreen";
 import ProjectScreen from "../Screen/ProjectScreen";
 import CertificateScreen from "../Screen/CertificateScreen";
 import ContactScreen from "../Screen/ContactScreen";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
   const [openScreens, setOpenScreens] = useState([]); // array of screen names
@@ -21,12 +22,30 @@ const Home = () => {
   };
 
   return (
-    <div className="w-full h-screen bg-blue-200 flex flex-col relative overflow-hidden">
-      <Navbar />
+    <div className="w-full h-screen bg-[linear-gradient(to_bottom,#FEF9C3_60%,#BFDBFE_40%)] flex flex-col relative overflow-hidden">
 
-      <main className="flex-1 flex items-center justify-center">
+      <Navbar />
+      <div className="flex flex-col w-full"></div>
+
+      <main className="flex-1 z-10 flex items-center justify-center">
         <HomeCard openScreen={openScreen} />
       </main>
+
+<div className="absolute z-0 bottom-0 left-0 w-full h-[40%] overflow-hidden pointer-events-none">
+  <Marquee
+    speed={20}
+    gradient={false}
+    autoFill
+    className="h-full"
+    pauseOnHover
+  >
+    <img
+      src="/images/cloud.webp" // long image already containing cloud1cloud2cloud3
+      alt="clouds"
+      className="h-120 w-auto object-contain block"
+    />
+  </Marquee>
+</div>
 
       {/* Render draggable screens */}
       {openScreens.includes("About Me") && <AboutMeScreen onClose={() => closeScreen("About Me")} />}
